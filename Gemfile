e537eaf46fb3f4f234e8719272a6ab7f7e8e2f35
Jenn Eng <jenneng6@gmail.com>
2017-02-10 15:12:32 -0500

    Add database cleaner

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
+
 group :development do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
@@ -20,6 +21,10 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.5'
   gem 'shoulda'
   gem 'valid_attribute'
+end
+
+group :test do
+  gem 'database_cleaner'
 end
 
 group :production do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,5 +60,24 @@ require "valid_attribute"
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-end
 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
